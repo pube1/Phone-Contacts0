1824264f9a6da0bf3c3dd321f8886d5f460975c1
Pube <Efehan@DESKTOP-66J4M3U>
2024-06-30 13:33:36 +0300

    Proje dosyası ekle.

---
+++ phoneDirectory/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data.OleDb;
+using System.IO;
+
+namespace phoneDirectory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SQLiteConnection conn = new SQLiteConnection("Data Source=phoneDirection.db; Version=3");
+        db db = new db();
+        
+        public void clearTextbox()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            maskedTextBox1.Clear();
+            deleteFindTb.Clear();
+            deleteIDTb.Clear();
+            updateIdTb.Clear();
+            updateNmbrMtb.Clear();
+            updateNmTb.Clear();
+            updateSearchTb.Clear();
+            updateSnTb.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db.name = textBox1.Text;
+            db.surname = textBox2.Text;
+            db.phoneNo = maskedTextBox1.Text;
+            db.email = textBox4.Text;
+            db.adress = textBox5.Text;
+            db.addContact(maskedTextBox1.Text);
+            dataGridView1.DataSource = db.showdata("select * from contacts");
+            clearTextbox();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.showdata("select * from contacts");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            db.id = deleteIDTb.Text;
+            db.deleteContact();
+            dataGridView1.DataSource = db.showdata("select * from contacts");
+            clearTextbox();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 1)
+            {
+                MessageBox.Show("Enter the ID of the person to be deleted for deletion");
+            }
+
+            else if (tabControl1.SelectedIndex == 2)
+            {
+                MessageBox.Show("To update, enter the ID of the person to update and enter the information to update");
+            }
+            clearTextbox();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.showdata("select * from contacts where name like" + "'" + "%" + textBox3.Text  + "%" + "'");
+        }
+
+        private void deleteFindTb_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.showdata("select * from contacts where name like" + "'" + "%" + deleteFindTb.Text + "%" + "'");
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.showdata("select * from contacts where name like" + "'" + "%" + updateSearchTb.Text + "%" + "'");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (updateIdTb.Text == "")
+            {
+                MessageBox.Show("Please fill in the blanks");
+                return;
+            }
+
+            db.name = updateNmTb.Text;
+            db.surname = updateSnTb.Text;
+            db.phoneNo = updateNmbrMtb.Text;
+            db.email = textBox6.Text;
+            db.adress = textBox7.Text;
+
+            db.updatecontact();
+
+            dataGridView1.DataSource = db.showdata("select * from contacts");
+            clearTextbox();
+
+        }
+    }
+}
---
+++ phoneDirectory/db.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace phoneDirectory
+{
+    internal class db
+    {
+        public string name;
+        public string surname;
+        public string phoneNo;
+        public string id;
+
+        public string email;
+        public string adress;
+
+        SQLiteConnection conn = new SQLiteConnection("Data Source=phoneDirection.db; Version=3");
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+
+        public void addContact(string mTb)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            cmd = new SQLiteCommand("select * from contacts",conn);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (Convert.ToString(dr["number"]) == mTb)
+                {
+                    MessageBox.Show("this number is registered to a person named " + dr["name"] + " " + dr["surname"]);
+                    dr.Close();
+                    conn.Close();
+                    return;
+                }
+            }
+            dr.Close();
+
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@surname", surname);
+            cmd.Parameters.AddWithValue("@phoneno", phoneNo);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@adress", adress);
+
+
+            cmd.CommandText = ("INSERT INTO contacts(name,surname,number) VALUES(@name,@surname,@phoneno)");
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = ("select * from contacts where number like @phoneno");
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                id = dr["id"].ToString();
+            }
+            dr.Close();
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.CommandText = ("INSERT INTO contacts_detail(contactId,email,adress) VALUES(@id, @email, @adress)");
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            MessageBox.Show("Registration done");
+        }
+
+        public void deleteContact()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            cmd.Connection= conn;
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.CommandText =("delete from contacts where id=@id");
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = ("select id from contacts_detail where contactId like @id");
+
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                id = Convert.ToString(dr["id"]);
+            }
+            dr.Close();
+            MessageBox.Show(id);
+            cmd.CommandText=("delete from contacts_detail where id="+id);
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Deletion has taken place");
+        }
+
+        public void updatecontact()
+        {
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }            
+
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@surname", surname);
+            cmd.Parameters.AddWithValue("@phoneno", phoneNo);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@adress", adress);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.CommandText = ("update contacts set name=@name, surname=@surname, number=@phoneno where id=@id");
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = ("select id from contacts_detail where contactId like @id");
+
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                id = Convert.ToString(dr["id"]);
+            }
+
+            dr.Close();
+            cmd.CommandText = ("update contacts_detail set email=@email, adress=@adress where id=@id");
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("Update has taken place");
+        }
+
+        public DataTable showdata(string data)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            cmd = new SQLiteCommand(data, conn);
+            dr = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(dr);
+            return table;
+            
+        }
+
+    }
+}
